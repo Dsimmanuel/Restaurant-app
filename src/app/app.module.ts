bbf21a277575057241250b5470d4af5ae7ed6f42
Dsimmanuel <dsimmanuel592@gmail.com>
2022-07-08 00:16:19 +0530

    completed component viewfood

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginpageComponent } from './loginpage/loginpage.component';
+import { ViewfoodComponent } from './viewfood/viewfood.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginpageComponent
+    LoginpageComponent,
+    ViewfoodComponent
   ],
   imports: [
     BrowserModule,
